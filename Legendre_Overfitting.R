c9d77a7230f1300dc112f27f8885f30522103318
apapiu <alex.papiu@gmail.com>
2016-02-21 19:00:11 -0600

    New function overfit.plot plus manipulate package

--- Legendre_Overfitting.R
+++ Legendre_Overfitting.R
@@ -1,37 +1,43 @@
 library(polynom)
+library(manipulate)
 
-q = 40 #degree of poly
-n = 25 #number of examples
-s = .5 #this is really sigma squared
-set.seed(132)
-x <- runif(n, min = -1, max = 1)
+#q is degree of polynomial, n is number of examples, s is sigma squared
+
+overfit.plot <- function(q,n,s) {
+    #set.seed(127)
+    x <- runif(n, min = -1, max = 1)
     
-poly <- polynomial(rnorm(n = q+1)) #poly
+    epsi <- rnorm(n) #noise
+    poly <- polynomial(rnorm(n = q+1)) #poly
+    y <- predict(poly, x) + sqrt(s)*epsi #values of poly +noise
+    
+    df <- data.frame(x, y)
+    
+    model_2 <- lm(y ~ poly(x,2), data = df)
+    model_10 <- lm(y ~ poly(x, 10), data = df)
+    
+    
+    test <- runif(2000, min = -1, max = 1)
+    testdf <- data.frame(test)
+    colnames(testdf)[1]<-"x"
+    newy_2 <- predict(model_2, newdata = testdf)
+    newy_10 <- predict(model_10, newdata = testdf)
+   
+    plot(test, predict(poly,test), ylim = c(-3,3), xlab = "x", ylab = "y", pch = 20 )
+    points(test, newy_10, col = "red", pch = 20)
+    points(test, newy_2, col = "green", pch = 20)
+    points(x, y, col = "dark blue")
+}
 
-epsi <- rnorm(n) #noise
-
-df <- data.frame(x, y)
-
-model_2 <- lm(y ~ poly(x,2), data = df)
-model_10 <- lm(y ~ poly(x, 10), data = df)
+manipulate(overfit.plot(q, n, s),
+           q = slider(min = 1, max = 20),
+           n = slider(min = 20, max = 200),
+           s = slider(min = .01, max = 2))
 
 
-#now we want E_out
-test <- runif(1000, min = -1, max = 1)
-testdf <- data.frame(test)
-colnames(testdf)[1]<-"x"
-plot(test, predict(poly,test), ylim = c(-2,3), xlab = "x", ylab = "y", sub = "$g_10$ outperforms $g_2$",
-     main = "Noisy 5th Degree Polynomial with 40 Data Points")
-newy_2 <- predict(model_2, newdata = testdf)
-newy_10 <- predict(model_10, newdata = testdf)
-points(test, newy_10, col = "red")
-points(test, newy_2, col = "green")
-points(x, y, col = "blue")
 
 Eout_2 <- sum
-
 Eout_2 <- sum((predict(poly,test)-newy_2)^2)/1000
-
 Eout_10 <- sum((predict(poly,test)-newy_10)^2)/1000
 
 
@@ -76,7 +82,6 @@ Q <- c(5, 10, 15, 20, 25, 30)
 N<- c(40, 60, 80, 100, 120, 140, 160)
 
 sigma <- c(0, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2)
-
 
 #Let's vary N and keep Q = 10, Sigma = .5
 trainexamples <- numeric(length(N))
